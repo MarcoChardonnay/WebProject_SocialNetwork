1ccf7eb324bbfa4f3e234dc696f24191f128b127
Marco Cardone <marco.cardone2@studio.unibo.it>
2023-12-08 15:19:22 +0100

    css added

--- template/base.php
+++ template/base.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- css -->
+    <link rel='stylesheet' type='text/css' href='css/style.css'>
     <!-- favicon -->
     <title>
         <? echo $templateParams["title"]; ?>
