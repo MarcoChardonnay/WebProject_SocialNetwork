66944ffdaf86429188b3c9a0cbf6556eba01f857
Marco Cardone <marco.cardone2@studio.unibo.it>
2024-01-15 19:01:49 +0100

    Update CSS styles and settings form layout

--- template/settings_form.php
+++ template/settings_form.php
@@ -1,31 +1,30 @@
 <div class="form-wrapper">
-    <p>Settings</p>
-    <p>Here you can modify your profile informations</p>
+    <h1>Here you can modify your profile informations</h1>
     <form class="form" action="registration.php" method="POST">
-        <p>Old username:</p>
+        <h2>Old username:</h2>
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" value="Test" required><br><br>
 
-        <p>Old password:</p>
+        <h2>Old password:</h2>
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br><br>
 
         <label for="confirmpass">Confirm New Password</label>
         <input type="password" id="confirmpass" name="confirmpass">
 
-        <p>Old email:</p>
+        <h2>Old email:</h2>
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br><br>
 
-        <p>Old phone:</p>
+        <h2>Old phone:</h2>
         <label for="phone">Phone:</label>
         <input type="text" id="phone" name="phone" pattern="[0-9]{10}" required><br><br>
 
-        <p>Previous setting:</p>
+        <h2>Previous setting:</h2>
         <label for="notification">Receive Notifications:</label>
         <input type="checkbox" id="notification" name="notification" value="1"><br><br>
 
-        <p>Old profile picture: [image]</p>
+        <h2>Old profile picture: [image]</h2>
         <label for="profile_picture">Profile Picture URL:</label>
         <input type="file" id="profile_picture" name="profile_picture"><br><br>
 
