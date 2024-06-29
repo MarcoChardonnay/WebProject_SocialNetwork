22696aa4a3423c18d9414897497ea2d8896f997f
Marco Cardone <marco.cardone2@studio.unibo.it>
2024-06-29 19:12:49 +0200

    fixed registration form

--- template/registration_form.php
+++ template/registration_form.php
@@ -1,25 +1,33 @@
 <div class="form-wrapper">
     <form class="form" action="registration.php" method="POST">
-        <label for="username">Username:</label>
-        <label for="username">The accepted characters are letters, numbers, period and underscores</label>
-        <input type="text" id="username" name="username" pattern="[a-zA-Z0-9_.]+" title="only alphanumeric characters underscores[_] and periods[.]" required><br><br>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-
-        <label for="confirmpass">Confirm Password:</label>
-        <input type="password" id="confirmpass" name="confirmpass"><br><br>
-
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
-
-        <label for="notification">Receive Notifications:</label>
-        <input type="checkbox" id="notification" name="notification" value="1"><br><br>
-
-        <label for="profile_picture">Profile Picture URL:</label>
-        <input type="file" id="profile_picture" name="profile_picture"><br><br>
-
-        <input type="submit" value="Register">
+        <ul>
+            <li>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" pattern="[a-zA-Z0-9._]+" title="Only alphanumeric characters, underscores [_], and periods [.] are allowed." aria-describedby="usernameHelp" required><br>
+                <span id="usernameHelp" class="form-descr">Use letters, numbers, periods, and underscores only. Must be under 30 characters. For example: john_doe.123</span>
+            </li>
+            <li>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required><br>
+                <span id="passwordHelp" class="form-descr">Use letters, numbers, and special characters only. For example: password123!.</span>
+            </li>
+            <li>
+                <label for="confirmpass">Confirm Password:</label>
+                <input type="password" id="confirmpass" name="confirmpass" required>
+            </li>
+            <li>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" aria-describedby="emailFeedback" required>
+                <div id="emailFeedback" aria-live="polite"></div>
+            </li>
+            <li>
+                <label for="notification">Receive Notifications:</label>
+                <input type="checkbox" id="notification" name="notification" value="1">
+            </li>
+            <li>
+                <input type="submit" value="Register">
+            </li>
+        </ul>
     </form>
     <p>Already registered? <a href="login.php">Go to login</a></p>
     <?php if (isset($templateParams["registrationerror"])) : ?>
