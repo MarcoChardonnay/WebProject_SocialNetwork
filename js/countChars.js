a3e697686e80070fd2b5c5c3ff53ee70f8a0c93b
Marco Cardone <marco.cardone2@studio.unibo.it>
2024-06-29 19:08:28 +0200

    added more characters per post

--- js/countChars.js
+++ js/countChars.js
@@ -4,10 +4,10 @@ window.onload = function() {
 
     inputElement.addEventListener('input', function() {
         var currentLength = inputElement.value.length;
-        counterElement.innerText = currentLength + '/255';
+        counterElement.innerText = currentLength + '/1500';
 
-        // Check if the length exceeds 255 and change color accordingly
-        if (currentLength > 255) {
+        // Check if the length exceeds 1500 and change color accordingly
+        if (currentLength > 1500) {
             counterElement.style.color = 'red';
         } else {
             counterElement.style.color = 'black'; // Reset to black if length is within limit
